a247d4ab4c87408a644feb9e9eb989712551426a
Liam Murray <liammurray404@gmail.com>
2024-01-19 23:52:05 -0800

    taking a break for now

---
+++ src/scenes/Menu.js
@@ -0,0 +1,17 @@
+class Menu extends Phaser.Scene {
+    constructor() {
+        super("menuScene")
+    }
+
+    preload () {
+        //load images
+        this.load.image('rocket', './assets/img/rocket.png')
+        this.load.image('spaceship', './assets/img/spaceship.png')
+        this.load.image('starfield', './assets/img/starfield.png')
+    }
+
+    create() {
+        this.add.text(20, 20, "Rocket Patrol Menu")
+        this.scene.start("playScene")
+    }
+}
